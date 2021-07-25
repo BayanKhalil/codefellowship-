d9d89d5f4eb47a6aa706ff14e3cc3dd02ca4ba59
Bayan Khalil <bayan.khalil92@yahoo.com>
2021-07-25 21:08:47 +0300

    lab16

---
+++ src/main/java/com/example/codefellowship/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.codefellowship;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        ApplicationUser user = applicationUserRepository.findUserByUsername(username);
+
+        if (user == null) {
+            System.out.print("Username not found");
+            throw new UsernameNotFoundException((username + " not found"));
+        }
+
+        return user;
+    }
+}
